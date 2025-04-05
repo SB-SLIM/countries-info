a8b5219b17c03a554a49f7a4126454c479c41efe
SLIM BOUCHOUCHA <bouchouchaslim@gmail.com>
2025-04-05 17:43:09 +0100

    Dev (#17)
    * ci: update tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: false,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
+  minify: true,
   format: ['cjs', 'esm'],
   dts: true,
   outDir: 'dist',
