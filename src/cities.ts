97b2e05ba3e5e640305c72a69122a38347e1f55d
Slim Bouchoucha <bouchouchaslim@gmail.com>
2025-04-05 14:11:05 +0100

    format: fix

--- src/cities.ts
+++ src/cities.ts
@@ -1,7 +1,6 @@
 import { citiesDb } from './data';
 import type { CitiesCode } from './types/common';
 
-
 export const getCitiesByCountry = (
   countryCode: CitiesCode,
   // locale: Locales = 'en',
