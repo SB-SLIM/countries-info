98c9efb997ea733da8304089636c1ef522b9a391
Slim Bouchoucha <bouchouchaslim@gmail.com>
2025-04-24 08:18:43 +0100

    format fix

--- src/utils/dirname/index.ts
+++ src/utils/dirname/index.ts
@@ -5,10 +5,15 @@ if (process.env.BUILD_FORMAT === 'esm') {
   import('./dirname.esm').then((module) => {
     __dirname = module.__dirname;
   });
-} else {
+} else if (process.env.BUILD_FORMAT === 'cjs') {
   // CJS-specific require
   const { __dirname: cjsDirname } = require('./dirname.cjs');
   __dirname = cjsDirname;
+} else {
+  // ESM-specific import (dynamic import for ESM)
+  import('./dirname.esm').then((module) => {
+    __dirname = module.__dirname;
+  });
 }
 
 export { __dirname };
